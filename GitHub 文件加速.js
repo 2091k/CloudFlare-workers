c6cd754264a8c7a5cf6d1f392046c8acd1b33eff
2091k <140614254+2091k@users.noreply.github.com>
2024-09-01 15:44:33 +0800

    Update GitHub 文件加速.js

--- GitHub 文件加速.js
+++ GitHub 文件加速.js
@@ -1,180 +1,130 @@
-'use strict'
-
-/**
- * static files (404.html, sw.js, conf.js)
- */
-const ASSET_URL = 'https://hunshcn.github.io/gh-proxy/'
-// 前缀，如果自定义路由为example.com/gh/*，将PREFIX改为 '/gh/'，注意，少一个杠都会错！
-const PREFIX = '/'
-// 分支文件使用jsDelivr镜像的开关，0为关闭，默认关闭
-const Config = {
-    jsdelivr: 0
-}
-
-const whiteList = [] // 白名单，路径里面有包含字符的才会通过，e.g. ['/username/']
-
-/** @type {ResponseInit} */
-const PREFLIGHT_INIT = {
-    status: 204,
-    headers: new Headers({
-        'access-control-allow-origin': '*',
-        'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
-        'access-control-max-age': '1728000',
-    }),
-}
-
-
-const exp1 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:releases|archive)\/.*$/i
-const exp2 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:blob|raw)\/.*$/i
-const exp3 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:info|git-).*$/i
-const exp4 = /^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+?\/.+$/i
-const exp5 = /^(?:https?:\/\/)?gist\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+$/i
-const exp6 = /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/tags.*$/i
-
-/**
- * @param {any} body
- * @param {number} status
- * @param {Object<string, string>} headers
- */
-function makeRes(body, status = 200, headers = {}) {
-    headers['access-control-allow-origin'] = '*'
-    return new Response(body, {status, headers})
-}
-
-
-/**
- * @param {string} urlStr
- */
-function newUrl(urlStr) {
-    try {
-        return new URL(urlStr)
-    } catch (err) {
-        return null
-    }
-}
-
-
-addEventListener('fetch', e => {
-    const ret = fetchHandler(e)
-        .catch(err => makeRes('cfworker error:\n' + err.stack, 502))
-    e.respondWith(ret)
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
 })
 
-
-function checkUrl(u) {
-    for (let i of [exp1, exp2, exp3, exp4, exp5, exp6]) {
-        if (u.search(i) === 0) {
-            return true
+function parseURL(url) {
+    let urlbody = url.substr(8);
+    urlbody = decodeURIComponent(urlbody.substr(urlbody.indexOf('/') + 1))
+    console.log("    Parsing: URLBody: " + urlbody)
+    let split_header_url = urlbody.lastIndexOf("/", urlbody.search("://"))
+    let real_url = urlbody.substr(split_header_url + 1)
+    if (!real_url) {
+        throw "Invalid real URL: " + urlbody
+    }
+    let headersbody = urlbody.substr(0, split_header_url)
+    console.log("    Parsing: Real URL: " + real_url)
+    console.log("    Parsing: Headers JSON: " + headersbody)
+    if (!headersbody) {
+        return {
+            url: real_url,
+            headers: {},
         }
     }
-    return false
+    if (!headersbody.startsWith("{")) {
+        headersbody = decodeURIComponent(headersbody)
+    }
+
+    if (!headersbody.startsWith("{")) {
+        throw "Invalid URL headers string: " + headersbody
+    }
+
+    headers = JSON.parse(headersbody)
+    
+    return {
+        url: real_url,
+        headers: headers,
+    }
 }
 
-/**
- * @param {FetchEvent} e
- */
-async function fetchHandler(e) {
-    const req = e.request
-    const urlStr = req.url
-    const urlObj = new URL(urlStr)
-    let path = urlObj.searchParams.get('q')
-    if (path) {
-        return Response.redirect('https://' + urlObj.host + PREFIX + path, 301)
+async function handleRequest(request) {
+    if (request.method == "OPTIONS") {
+        return new Response("", {status:200, headers:{
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Max-Age": "31536000",
+            "X-Request-Type": "CORS Preflight"
+        }});
     }
-    // cfworker 会把路径中的 `//` 合并成 `/`
-    path = urlObj.href.substr(urlObj.origin.length + PREFIX.length).replace(/^https?:\/+/, 'https://')
-    if (path.search(exp1) === 0 || path.search(exp5) === 0 || path.search(exp6) === 0 || path.search(exp3) === 0 || path.search(exp4) === 0) {
-        return httpHandler(req, path)
-    } else if (path.search(exp2) === 0) {
-        if (Config.jsdelivr) {
-            const newUrl = path.replace('/blob/', '@').replace(/^(?:https?:\/\/)?github\.com/, 'https://cdn.jsdelivr.net/gh')
-            return Response.redirect(newUrl, 302)
+    
+    let reqHeaders = new Headers(request.headers),
+        outBody, outStatus = 200, outCt = null, outHeaders = new Headers({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": reqHeaders.get('Access-Control-Allow-Headers') || "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token"
+        });
+
+    try {
+        console.log("Got Raw ReqUrl: " + request.url)
+        let t = parseURL(request.url)
+        let url = t.url
+        let headers = t.headers
+        console.log("Parsed URL body: " + url)
+        console.log("Parsed URL headers: " + JSON.stringify(headers))
+
+        if (url.length < 3 || url.indexOf('.') == -1) {
+            throw "invalid URL input: " + url;
+        } else if (url == "favicon.ico" || url == "robots.txt") {
+            return Response.redirect('https://workers.cloudflare.com', 307)
         } else {
-            path = path.replace('/blob/', '/raw/')
-            return httpHandler(req, path)
+            if (url.toLowerCase().indexOf("http") == -1) {
+                url = "http://" + url;
+            }
+
+            let fp = {
+                method: request.method,
+                headers: {}
+            }
+
+            let he = reqHeaders.entries();
+            for (let h of he) {
+                if (!['content-length', 'content-type'].includes(h[0])) {
+                    fp.headers[h[0]] = h[1];
+                }
+            }
+
+            if (headers["_method"] !== undefined) {
+                fp.method = headers["_method"].toUpperCase()
+            }
+            fp.headers = Object.assign({}, fp.headers, headers)
+            
+            if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
+                const ct = (reqHeaders.get('content-type') || "").toLowerCase();
+                if (ct.includes('application/json')) {
+                    fp.body = JSON.stringify(await request.json());
+                } else if (ct.includes('application/text') || ct.includes('text/html')) {
+                    fp.body = await request.text();
+                } else if (ct.includes('form')) {
+                    fp.body = await request.formData();
+                } else {
+                    fp.body = await request.blob();
+                }
+            }
+            if (headers["_body"] !== undefined) {
+                fp.body = headers["_body"]
+            }
+
+            let fr = (await fetch(url, fp));
+            outStatus = fr.status;
+            outCt = fr.headers.get('content-type');
+            outBody = fr.body;
         }
-    } else if (path.search(exp4) === 0) {
-        const newUrl = path.replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, '@$1').replace(/^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com/, 'https://cdn.jsdelivr.net/gh')
-        return Response.redirect(newUrl, 302)
-    } else {
-        return fetch(ASSET_URL + path)
-    }
-}
-
-
-/**
- * @param {Request} req
- * @param {string} pathname
- */
-function httpHandler(req, pathname) {
-    const reqHdrRaw = req.headers
-
-    // preflight
-    if (req.method === 'OPTIONS' &&
-        reqHdrRaw.has('access-control-request-headers')
-    ) {
-        return new Response(null, PREFLIGHT_INIT)
+    } catch (err) {
+        outStatus = 500
+        outCt = "application/json";
+        outBody = JSON.stringify({
+            code: -1,
+            msg: JSON.stringify(err.stack) || err
+        });
     }
 
-    const reqHdrNew = new Headers(reqHdrRaw)
-
-    let urlStr = pathname
-    let flag = !Boolean(whiteList.length)
-    for (let i of whiteList) {
-        if (urlStr.includes(i)) {
-            flag = true
-            break
-        }
+    if (outCt && outCt != "") {
+        outHeaders.set("content-type", outCt);
     }
-    if (!flag) {
-        return new Response("blocked", {status: 403})
-    }
-    if (urlStr.search(/^https?:\/\//) !== 0) {
-        urlStr = 'https://' + urlStr
-    }
-    const urlObj = newUrl(urlStr)
 
-    /** @type {RequestInit} */
-    const reqInit = {
-        method: req.method,
-        headers: reqHdrNew,
-        redirect: 'manual',
-        body: req.body
-    }
-    return proxy(urlObj, reqInit)
-}
-
-
-/**
- *
- * @param {URL} urlObj
- * @param {RequestInit} reqInit
- */
-async function proxy(urlObj, reqInit) {
-    const res = await fetch(urlObj.href, reqInit)
-    const resHdrOld = res.headers
-    const resHdrNew = new Headers(resHdrOld)
-
-    const status = res.status
-
-    if (resHdrNew.has('location')) {
-        let _location = resHdrNew.get('location')
-        if (checkUrl(_location))
-            resHdrNew.set('location', PREFIX + _location)
-        else {
-            reqInit.redirect = 'follow'
-            return proxy(newUrl(_location), reqInit)
-        }
-    }
-    resHdrNew.set('access-control-expose-headers', '*')
-    resHdrNew.set('access-control-allow-origin', '*')
-
-    resHdrNew.delete('content-security-policy')
-    resHdrNew.delete('content-security-policy-report-only')
-    resHdrNew.delete('clear-site-data')
-
-    return new Response(res.body, {
-        status,
-        headers: resHdrNew,
+    return new Response(outBody, {
+        status: outStatus,
+        headers: outHeaders
     })
 }
